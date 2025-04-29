3cbfde1846d6fb6d534b7dd320de85bddf5682cf
Omid Hashemzadeh <omidh2015@gmail.com>
2025-04-29 21:40:48 +0100

    Add Rating and comments functionality

--- pictures_app/models.py
+++ pictures_app/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class PictureModel(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    caption = models.TextField(null=True)
-    location = models.CharField(max_length=255, null=True)
+    caption = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    people_present = models.CharField(max_length=255, null=True, blank=True)
     picture_url = models.URLField()
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ForeignKey(PictureModel, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.picture.title}"
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ForeignKey(PictureModel, on_delete=models.CASCADE, related_name='ratings')
+    score = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Rating {self.score} by {self.user.username} on {self.picture.title}"
--- pictures_app/views.py
+++ pictures_app/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, redirect
-from .models import PictureModel
-from .utils import upload_picture_to_azure
-from django.http import HttpResponse
-from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect, get_object_or_404
+
+from pictures_app.utils import upload_picture_to_azure
+from .models import PictureModel, Comment, Rating
+from .forms import PictureForm, CommentForm, RatingForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
+from django.contrib.auth.decorators import login_required
+from .models import PictureModel, Comment, Rating
+from .forms import CommentForm, RatingForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
+
+
 
 @login_required
 def upload_picture(request):
@@ -16,24 +23,55 @@ def upload_picture(request):
         # e.g : mypic01.jpg -> mypic01
         name :str = request.FILES['picture'].name
 
-        # Save metadata to the database     # TODO : uncomment the below and use them (instead of hard-coded values)
-        # title = request.POST.get('title')
-        # caption = request.POST.get('caption')
-        # location = request.POST.get('location')
-
+        caption = request.POST.get('caption')
+        location = request.POST.get('location')
         title = name
-        caption = f"Very Beautiful picture"
-        location = "UK"
+        creator = request.user
 
-        PictureModel.objects.create(title=title, caption=caption, location=location, picture_url=picture_url)
+        PictureModel.objects.create(title=title, caption=caption, location=location, picture_url=picture_url, creator=creator)
         messages.success(request, 'Picture uploaded successfully!')
         return redirect('view_galleries')
     return render(request, 'upload_picture.html')
 
+
+
+
+@login_required
 def view_galleries(request):
+    if request.method == 'POST':
+        picture_id = request.POST.get('picture_id')
+        picture = PictureModel.objects.get(id=picture_id)
+
+        if 'rating_submit' in request.POST:
+            rating_form = RatingForm(request.POST)
+            if rating_form.is_valid():
+                rating = rating_form.save(commit=False)
+                rating.user = request.user
+                rating.picture = picture
+                rating.save()
+
+        elif 'comment_submit' in request.POST:
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.user = request.user
+                comment.picture = picture
+                comment.save()
+
+        return redirect('view_galleries')
+
     pictures = PictureModel.objects.all()
-    return render(request, 'view_galleries.html', {'pictures': pictures})
+    enriched_pictures = []
 
+    for pic in pictures:
+        average_rating = pic.ratings.aggregate(Avg('score'))['score__avg']
+        comments = pic.comments.order_by('-timestamp')
+        enriched_pictures.append({
+            'picture': pic,
+            'average_rating': average_rating,
+            'rating_form': RatingForm(),
+            'comment_form': CommentForm(),
+            'comments': comments
+        })
 
-def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'view_galleries.html', {'gallery': enriched_pictures})
